df4afca062b0dd832b622ef0f39518aae2a75271
telfer17 <davietelfer@hotmail.com>
2021-02-19 16:09:36 +0000

    added image folders and data

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -13,7 +13,9 @@ const Hero = () => {
         <HeroItems>
           <HeroH1>Unreal Destinations</HeroH1>
           <HeroP>Out of this world!</HeroP>
-          <Button>Travel Now</Button>
+          <Button primary="true" big="true" round="true" to="/trips">
+            Travel Now
+          </Button>
         </HeroItems>
       </HeroContent>
     </HeroContainer>
@@ -32,11 +34,73 @@ const HeroContainer = styled.div`
   position: relative;
   margin-top: --80px;
   color: #fff;
-`
-const HeroBg = styled.div``
 
-const VideoBg = styled.video``
-const HeroContent = styled.div``
-const HeroItems = styled.div``
-const HeroH1 = styled.div``
-const HeroP = styled.div``
+  :before {
+    content: '';
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    left: 0;
+    z-index: 2;
+    background: linear-gradient(
+        180deg,
+        rgba(0, 0, 0, 0.2) 0%,
+        rgba(0, 0, 0, 0.6) 100%
+      ),
+      linear-gradient(180deg, rgba(0, 0, 0, 0.2) 0%, transparent 100%);
+  }
+`
+const HeroBg = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+`
+
+const VideoBg = styled.video`
+  width: 100%;
+  height: 100%;
+  -o-object-fit: cover;
+  object-fit: cover;
+`
+
+const HeroContent = styled.div`
+  z-index: 3;
+  height: calc(100vh - 80px);
+  max-height: 100;
+  padding: 0rem calc((100vw - 1300px) / 2);
+`
+
+const HeroItems = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  height: 100vh;
+  max-height: 100%;
+  padding: 0;
+  color: #fff;
+  line-height: 1.1;
+  font-weight: bold;
+  font-weight: bold;
+`
+
+const HeroH1 = styled.div`
+  font-size: clamp(1.5rem, 6vw, 4rem);
+  margin-bottom: 1.5rem;
+  letter-spacing: 3px;
+  font-weight: bold;
+  padding: 0 1rem;
+`
+
+const HeroP = styled.div`
+  font-size: clamp(1rem, 3vw, 3rem);
+  margin-bottom: 2rem;
+  font-weight: 400;
+`
